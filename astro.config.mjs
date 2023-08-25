b43ab3874d584030c7e93c9db03396f1c176992e
SiffKL <Siff0407@stud.kea.dk>
2023-08-25 15:18:13 +0200

    trytoimporticon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({
-    experimental: {
-        assets: true,
-      },
-});
 
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        mdi: ["*"], // Loads entire Material Design Icon set
+      },
+    }),
+  ],
+});
